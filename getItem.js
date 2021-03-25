bcf955892e1f7f8af915d61df96fd1b77ac9fa2d
sho101651 <sho.junurairat@hotmail.com>
2021-03-25 22:01:19 +0700

    create function getItem

---
+++ getItem.js
@@ -0,0 +1,18 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableProduct,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key:{productId: event.pathParameters.id}, // The id of the note from the path
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
